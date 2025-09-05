10baa74fe9778ac125ea7a0f85a49f06c507d9f5
nikhil chouksey <nikhilranger18@gmail.com>
2025-09-05 10:44:07 +0530

    222. Count Complete Tree Nodes

---
+++ Binary_tree/222. Count Complete Tree Nodes.py
@@ -0,0 +1,80 @@
+# Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+# According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+# Design an algorithm that runs in less than O(n) time complexity.
+
+#  Example 1:
+# Input: root = [1,2,3,4,5,6]
+# Output: 6
+
+# Example 2:
+# Input: root = []
+# Output: 0
+
+# Example 3:
+# Input: root = [1]
+# Output: 1
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+
+#bfs approach 
+
+        # total_nodes = 0 
+
+        # if not root : 
+        #     return total_nodes 
+        
+        # queue = deque([root])
+
+        # while queue: 
+        #     size = len(queue)
+        #     level = []
+        #     for _ in range(size): 
+        #         node = queue.popleft()
+        #         if node.left : 
+        #             queue.append(node.left)
+        #         if node.right : 
+        #             queue.append(node.right)
+        #         level.append(node.val)
+        #     total_nodes += len(level)
+        
+        # return total_nodes
+    
+
+#dfs approach 
+
+        if not root : 
+            return 0 
+        
+        def lHeight(node):
+            h = 0  
+            while node : 
+                node = node.left
+                h += 1 
+            return h 
+        
+        def rHeight(node): 
+            h = 0 
+            while node : 
+                node = node.right 
+                h+=1 
+            return h 
+        
+        lH = lHeight(root)
+        rH = rHeight(root)
+
+        if lH == rH : 
+            return (1<<lH) - 1
+        
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+            
+
+            
