c58aaeb7010a19adb519e6a8bed16b14ab803fb8
nikhil chouksey <nikhilranger18@gmail.com>
2025-09-28 16:02:23 +0530

    subsets

---
+++ recursion/78. Subsets.py
@@ -0,0 +1,25 @@
+# Given an integer array nums of unique elements, return all possible subsets (the power set).
+# The solution set must not contain duplicate subsets. Return the solution in any order.
+
+# Example 1:
+# Input: nums = [1,2,3]
+# Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+# Example 2:
+# Input: nums = [0]
+# Output: [[],[0]]
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        def generate_subsets(nums,index=0 , curr = []): 
+            if index == len(nums): 
+                result.append(curr)
+                return 
+            
+            generate_subsets(nums,index+1,curr) 
+
+            generate_subsets(nums,index+1,curr + [nums[index]])
+        
+        generate_subsets(nums,0,[])
+        return result
