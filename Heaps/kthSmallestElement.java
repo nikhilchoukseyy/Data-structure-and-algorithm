2af979d9d4a31d233cbf3481e70f1e196793ddca
nikhil chouksey <nikhilranger18@gmail.com>
2025-10-03 20:15:09 +0530

    kth smallest Element

---
+++ Heaps/kthSmallestElement.java
@@ -0,0 +1,37 @@
+// 378. Kth Smallest Element in a Sorted Matrix
+
+// Given an n x n matrix where each of the rows and columns is sorted in ascending order, return the kth smallest element in the matrix.
+// Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+// You must find a solution with a memory complexity better than O(n2).
+// Example 1
+// Input: matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
+// Output: 13
+// Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8th smallest number is 13
+// Example 2:
+// Input: matrix = [[-5]], k = 1
+// Output: -5
+import java.util.*;
+
+class kthSmallestElement {
+
+    public static int kthSmallestElement(int[][] matrix, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int[] row : matrix) {
+            for (int num : row) {
+                maxHeap.add(num);
+                if (k < maxHeap.size()) {
+                    maxHeap.poll();
+                }
+            }
+        }
+
+        return maxHeap.peek();
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 5, 9}, {10, 11, 13}, {12, 13, 15}};
+        int k = 8;
+        System.out.println(kthSmallestElement(matrix, k));
+    }
+}
--- Heaps/minToMaxHeap.java
+++ Heaps/minToMaxHeap.java
@@ -1,3 +1,18 @@
+// Given a min-heap in array representation named nums, convert it into a max-heap and return the resulting array.
+// A min-heap is a complete binary tree where the key at the root is the minimum among all keys present in a binary min-heap and the same property is recursively true for all nodes in the Binary Tree.
+// A max-heap is a complete binary tree where the key at the root is the maximum among all keys present in a binary max-heap and the same property is recursively true for all nodes in the Binary Tree.
+// Since there can be multiple answers, the compiler will return true if it's correct, else false.
+
+
+// Examples:
+// Input: nums = [10, 20, 30, 21, 23]
+// Output: [30, 21, 23, 10, 20]
+// Explanation:
+
+// Input: nums = [-5, -4, -3, -2, -1]
+// Output: [-1, -2, -3, -4, -5]
+// Explanation:
+
 import java.util.* ; 
 
 class minToMaxHeap{
