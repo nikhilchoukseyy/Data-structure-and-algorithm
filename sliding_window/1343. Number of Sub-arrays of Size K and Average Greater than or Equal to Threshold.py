d5c8cd2ea0e09ea4a48c88a4ad882599c9d15276
nikhil chouksey <nikhilranger18@gmail.com>
2025-10-26 10:40:45 +0530

    1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold

---
+++ sliding_window/1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.py
@@ -0,0 +1,30 @@
+# Given an array of integers arr and two integers k and threshold, return the number of sub-arrays of size k and average greater than or equal to threshold.
+
+# Example 1:
+# Input: arr = [2,2,2,2,5,5,5,8], k = 3, threshold = 4
+# Output: 3
+# Explanation: Sub-arrays [2,5,5],[5,5,5] and [5,5,8] have averages 4, 5 and 6 respectively. All other sub-arrays of size 3 have averages less than 4 (the threshold).
+
+# Example 2:
+# Input: arr = [11,13,17,23,29,31,7,5,2,3], k = 3, threshold = 5
+# Output: 6
+# Explanation: The first 6 sub-arrays of size 3 have averages greater than 5. Note that averages are not integers.
+
+def numOfSubarrays(arr,k,threshold): 
+  n = len(arr)
+  count = 0 
+  l = 0 
+  wS = 0 
+  
+  for r in range(n): 
+    wS += arr[r]
+    if r >= (k-1): 
+      avg = wS/k 
+      if avg > threshold: 
+        count +=1 
+      wS -= arr[l]
+      l+=1 
+    
+  return count
+
+print(numOfSubarrays(arr = [11,13,17,23,29,31,7,5,2,3], k = 3, threshold = 5))
